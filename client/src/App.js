e2299c3f41ae88e1747cb164736196804794ec69
Shane Chambry <Shaneechambry@gmail.com>
2018-10-13 11:00:35 -0700

    dependencies fix and clockIn button

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,8 @@ class App extends Component {
     this.getUser = this.getUser.bind(this)
     this.componentDidMount = this.componentDidMount.bind(this)
     this.updateUser = this.updateUser.bind(this)
+    this.clockIn = this.clockIn.bind(this)
+
 
   }
 
