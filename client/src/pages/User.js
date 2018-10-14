6235afa21173d6864267387d091bf030cc1da4a4
Shane Chambry <Shaneechambry@gmail.com>
2018-10-14 14:25:31 -0700

    updated favicion

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -39,10 +39,10 @@ class User extends Component {
       <div>
         <div>
           <h2>
-Welcome,
+            Welcome,
             {' '}
             {user.firstname1}
-!
+            !
           </h2>
           <div className="row">
 
@@ -52,27 +52,27 @@ Welcome,
               </Grid>
               <Grid item xs={3}>
                 <ul>
-                                    Your Schedule:
+                  Your Schedule:
                   <li>
-Monday: 8AM-5PM
+                    Monday: 8AM-5PM
                   </li>
                   <li>
-Tuesday: 8AM-5PM
+                    Tuesday: 8AM-5PM
                   </li>
                   <li>
-Wednesday: 8AM-5PM
+                    Wednesday: 8AM-5PM
                   </li>
                   <li>
-Thursday: 8AM-5PM
+                    Thursday: 8AM-5PM
                   </li>
                   <li>
-Fiday: 8AM-5PM
+                    Fiday: 8AM-5PM
                   </li>
                   <li>
-Saturday: Off
+                    Saturday: Off
                   </li>
                   <li>
-Sunday: Off
+                    Sunday: Off
                   </li>
                 </ul>
               </Grid>
@@ -86,70 +86,39 @@ Sunday: Off
 
           </div>
 
-<<<<<<< HEAD
-                    <div className="row" style={styles1}>
-                        <Grid container spacing={40} justify="space-evenly">
-                            <Grid item xs={4} >
-                                <Admin></Admin>
-                            </Grid>
-                            <Grid item xs={4} >
-                                <h1>Clock IN/OUT</h1>
-                                {this.props.status}
-                                <p id="ClockIN/OUT">{this.props.status ? ( 
-                                    <div>
-                                        You are clocked in.
-                                        <br/>
-                                <Button onClick={this.props.clockOut} style={styles2} >
-                                    Clock Out
-                         </Button>
-                                    </div>
-                                 ): ( 
-                                    <div>
-                                    You are clocked out.
-                                    <br/>
-                                <Button onClick={this.props.clockIn} style={styles3}  >
-                                    Clock In
-                        </Button>
-                                    </div>
-                                  )}</p>
-                            </Grid>
-                        </Grid>
-                        <Dashboard/>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-=======
           <div className="row" style={styles1}>
             <Grid container spacing={40} justify="space-evenly">
-              <Grid item xs={4}>
-                {/* <h6>CEO / CO-FOUNDER</h6>
-                                <h4>Keith Jones</h4>  */}
-                <Admin />
+              <Grid item xs={4} >
+                <Admin></Admin>
               </Grid>
-              <Grid item xs={4}>
+              <Grid item xs={4} >
                 <h1>Clock IN/OUT</h1>
-                <p id="ClockIN/OUT"> See Your Clock Status Here!! </p>
-                <Button onClick={this.props.clockIn} style={styles3}>
-                                    Clock In
-                </Button>
-                <br />
-                <br />
-                <br />
-                <Button onClick={() => { document.getElementById('ClockIN/OUT').innerHTML = `${'You Are Now Clocked OUT @' + ('<br />')}${this.state.time}`; }} style={styles2}>
-                                    Clock Out
-                </Button>
+                {this.props.status}
+                <p id="ClockIN/OUT">{this.props.status ? (
+                  <div>
+                    You are clocked in.
+                                        <br />
+                    <Button onClick={this.props.clockOut} style={styles2} >
+                      Clock Out
+                         </Button>
+                  </div>
+                ) : (
+                    <div>
+                      You are clocked out.
+                                    <br />
+                      <Button onClick={this.props.clockIn} style={styles3}  >
+                        Clock In
+                        </Button>
+                    </div>
+                  )}</p>
               </Grid>
             </Grid>
-            {/* </Grid> */}
             <Dashboard />
           </div>
         </div>
       </div>
     );
   }
->>>>>>> ea8f2136db950c9465bf0ac7effd73dfaaa7748c
 }
 
 export default User;
