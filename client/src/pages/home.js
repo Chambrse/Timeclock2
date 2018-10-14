a1304e92181449079318729a255bead972af6b8d
Angel Lugo <this@angellugo.com>
2018-10-14 09:45:16 -0700

    Refactored according to eslint. Some fixes are still open

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,72 +1,70 @@
-import React, { Component } from 'react'
-import { Button, Icon, Grid } from '@material-ui/core'
-import { Redirect } from 'react-router-dom'
-import { Route, Link } from 'react-router-dom'
-import color from '@material-ui/core/colors/pink';
-import axios from 'axios'
+import React, { Component } from 'react';
+import { Button, /* Icon, */ Grid } from '@material-ui/core';
+// import { Redirect } from 'react-router-dom';
+// import { Route, Link } from 'react-router-dom';
+// import color from '@material-ui/core/colors/pink';
+// import axios from 'axios';
 
 const styles1 = {
-    color: 'Black',
-    textAlign: 'center',
-    backgroundColor: 'Green',
-    fontWeight: 'bold',
-    padding: '20px'
+  color: 'Black',
+  textAlign: 'center',
+  backgroundColor: 'Green',
+  fontWeight: 'bold',
+  padding: '20px',
 };
 const styles2 = {
-    color: 'Black',
-    textAlign: 'center',
-    backgroundColor: 'Yellow',
-    fontWeight: 'bold',
-    padding: '20px'
+  color: 'Black',
+  textAlign: 'center',
+  backgroundColor: 'Yellow',
+  fontWeight: 'bold',
+  padding: '20px',
 };
 
 class Home extends Component {
-    constructor() {
-        super()
-    
-this.routeChange = this.routeChange.bind(this);
-this.routeChange2 = this.routeChange2.bind(this);
-   
-   }
-   routeChange(){
-    let path = "/login";
-    this.props.history.push(path);
-   }
-   
-   routeChange2(){
-    let path = "/signup";
-    this.props.history.push(path);
-    }
-    
-    render() {
+  constructor() {
+    super();
 
-        const imageStyle = {
-            width: 300,
-            height:200,
-            
-        }
-        
-            return (
-            <div >
-                
-                   
-                 <p >Welcome to</p>
-                   <Grid item xs={12}>
-                <img style={imageStyle} src={ require('../TC9000.jpg') }></img>
-                   </Grid>
-                   <br ></br>
-                   <p>Please click the Log-in or Sign-Up Button below to be redirected to the appropriate page.</p>
-                   <Button onClick={this.routeChange}style={styles1}>
+    this.routeChange = this.routeChange.bind(this);
+    this.routeChange2 = this.routeChange2.bind(this);
+  }
+
+  routeChange() {
+    const path = '/login';
+    this.props.history.push(path);
+  }
+
+  routeChange2() {
+    const path = '/signup';
+    this.props.history.push(path);
+  }
+
+  render() {
+    const imageStyle = {
+      width: 300,
+      height: 200,
+
+    };
+
+    return (
+      <div>
+
+
+        <p>Welcome to</p>
+        <Grid item xs={12}>
+          <img style={imageStyle} src={require('../TC9000.jpg')} />
+        </Grid>
+        <br />
+        <p>Please click the Log-in or Sign-Up Button below to be redirected to the appropriate page.</p>
+        <Button onClick={this.routeChange} style={styles1}>
                        Login
-                   </Button>&emsp;
-                   <Button onClick={this.routeChange2}style={styles2}>
+        </Button>&emsp;
+        <Button onClick={this.routeChange2} style={styles2}>
                        Sign-Up
-                   </Button>
-            </div>
-        )
-        }
+        </Button>
+      </div>
+    );
+  }
+}
 
-    }
 
-
-export default Home
+export default Home;
