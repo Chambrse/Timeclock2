f0647823e6115de99c34d20ad3d8b19fd507f8cc
gladavis89 <gla.davis89@gmail.com>
2018-10-23 11:49:00 -0700

    gd merged

--- client/src/components/AddUserModal.js
+++ client/src/components/AddUserModal.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-import ReactDOM from 'react-dom';
 import {
-  Button, TextField, Select, MenuItem, InputLabel, FormControl,
+  Button, TextField, Select, MenuItem, InputLabel, FormControl, Grid,
 } from '@material-ui/core';
 import Modal from 'react-responsive-modal';
-import PropTypes from 'prop-types';
-
-const pstyle = {
-  color: 'red',
-  margin: '0px',
-};
 
 class AddUserModal extends React.Component {
-  constructor(props) {
-    const { adminUsername } = props;
+  constructor() {
     super();
     this.state = {
       open: false,
-      adminUsername,
       username: '',
       usernameErrors: [],
       companyName: '',
@@ -47,167 +38,148 @@ class AddUserModal extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  onOpenModal = () => {
-    this.setState({ open: true });
-  };
+    onOpenModal = () => {
+      this.setState({ open: true });
+    };
 
-  onCloseModal = () => {
-    this.setState({ open: false });
-  };
+    onCloseModal = () => {
+      this.setState({ open: false });
+    };
 
-  handleSubmit(event) {
-    console.log('sign-up handleSubmit, username: ');
-    console.log(this.state.username);
-    event.preventDefault();
+    handleSubmit(event) {
+      console.log('sign-up handleSubmit, username: ');
+      console.log(this.username);
+      event.preventDefault();
 
-    // request to server to add a new username/password
-    axios.post('/addDelete/', this.state)
-      .then((response) => {
-        console.log('test');
+      // request to server to add a new username/password
+      axios.post('/addDelete/', this.state)
+        .then((response) => {
+          console.log('response', response);
 
-        if (response.data.errors) {
-          const newErrorsObj = {
-            companyNameErrors: [],
-            adminLastNameErrors: [],
-            cityErrors: [],
-            countryErrors: [],
-            postalCodeErrors: [],
-            employeeTypeErrors: [],
-            passwordErrors: [],
-            passwordMatchErrors: [],
-          };
+          if (response.data.errors) {
+            const newErrorsObj = {
+              usernameErrors: [],
+              emailErrors: [],
+              adminFirstNameErrors: [],
+              adminLastNameErrors: [],
+              employeeTypeErrors: [],
+              passwordErrors: [],
+              passwordMatchErrors: [],
+            };
 
-          Object.keys(response.data).forEach((key) => {
-            console.log([key]);
-            if ([key].toString() !== 'errors') {
-              response.data[key].forEach((element) => {
-                newErrorsObj[key].push(element.msg);
-              });
-            }
-          });
-          this.setState(newErrorsObj);
-        } else {
-          this.setState({ open: false });
-        }
-      }).catch((error) => {
-        console.log('signup error: ');
-        console.log(error);
+            Object.keys(response.data).forEach((key) => {
+              console.log([key]);
+              if ([key].toString() !== 'errors') {
+                response.data[key].forEach((element) => {
+                  newErrorsObj[key].push(element.msg);
+                });
+              }
+            });
+            this.setState(newErrorsObj);
+          } else {
+            this.setState({ open: false });
+          }
+        }).catch((error) => {
+          console.log('signup error: ');
+          console.log(error);
+        });
+    }
+
+
+    handleChange(event) {
+      this.setState({
+        [event.target.name]: event.target.value,
       });
-  }
+    }
 
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  render() {
-    const { open } = this.state;
-    const newLocal = this;
-    return (
-      <div>
-        <Button className="btn btn-success" onClick={this.onOpenModal}>Add Employee</Button>
-        <Modal open={open} onClose={this.onCloseModal} center>
-          <div className="SignupForm">
-            <h4>Add New Employee</h4>
-            <form className="form-horizontal">
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label" htmlFor="username">Username</label>
-                </div>
-                <div className="col-6 col-mr-auto">
+    render() {
+      const {
+        username, usernameErrors,
+        email, emailErrors,
+        adminFirstName, adminFirstNameErrors,
+        adminLastName, adminLastNameErrors,
+        password, passwordErrors,
+        passwordMatch, passwordMatchErrors,
+        employeeTypeErrors, open,
+        employeeType,
+      } = this.state;
+      return (
+        <div>
+          <Button color="primary" variant="contained" onClick={this.onOpenModal}>Add Employee</Button>
+          <Modal open={open} onClose={this.onCloseModal} center>
+            <div className="SignupForm">
+              <h4>Add New Employee</h4>
+              <form className="form-horizontal">
+                <Grid xs={12}>
                   <TextField
-                    className="form-input"
+                    margin="normal"
                     type="text"
                     id="username"
                     name="username"
-                    placeholder="Username"
-                    value={this.state.username}
+                    label="Username"
+                    value={username}
                     onChange={this.handleChange}
+                    error={usernameErrors > 0}
+                    helperText={usernameErrors.length > 0 ? (
+                      usernameErrors[0]
+                    ) : (null)
+                    }
                   />
-                  {this.state.usernameErrors.length > 0 ? (
-                    newLocal.state.usernameErrors.map((element, i) => <p style={pstyle} key={i}>{element}</p>)
-                  ) : console.log('it was false')
-                  }
-                </div>
-
-              </div>
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label" htmlFor="email">Email</label>
-                </div>
-                <div className="col-6 col-mr-auto">
+                </Grid>
+                <Grid xs={12}>
                   <TextField
-                    className="form-input"
+                    margin="normal"
                     type="text"
                     id="email"
                     name="email"
-                    placeholder="email"
-                    value={this.state.email}
+                    label="email"
+                    value={email}
                     onChange={this.handleChange}
+                    error={emailErrors > 0}
+                    helperText={emailErrors.length > 0 ? (
+                      emailErrors[0]
+                    ) : (null)
+                    }
                   />
-                  {this.state.emailErrors.length > 0 ? (
-                    this.state.emailErrors.map((element, i) => <p style={pstyle} key={i}>{element}</p>)
-                  ) : console.log('it was false')
-                  }
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label" htmlFor="adminFirstName">First Name</label>
-                </div>
-                <div className="col-6 col-mr-auto">
+                </Grid>
+                <Grid xs={12}>
                   <TextField
-                    className="form-input"
+                    margin="normal"
                     type="text"
                     id="adminFirstName"
                     name="adminFirstName"
-                    placeholder="first name"
-                    value={this.state.adminFirstName}
+                    label="first name"
+                    value={adminFirstName}
                     onChange={this.handleChange}
+                    error={adminFirstNameErrors > 0}
+                    helperText={adminFirstNameErrors.length > 0 ? (
+                      adminFirstNameErrors[0]
+                    ) : (null)
+                    }
                   />
-                  {this.state.adminFirstNameErrors.length > 0 ? (
-                    this.state.adminFirstNameErrors.map((element, i) => <p style={pstyle} key={i}>{element}</p>)
-                  ) : console.log('it was false')
-                  }
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label" htmlFor="adminLastName">Last Name</label>
-                </div>
-                <div className="col-6 col-mr-auto">
+                </Grid>
+                <Grid xs={12}>
                   <TextField
-                    className="form-input"
+                    margin="normal"
                     type="text"
                     id="adminLastName"
                     name="adminLastName"
-                    placeholder="last name"
-                    value={this.state.adminLastName}
+                    label="last name"
+                    value={adminLastName}
                     onChange={this.handleChange}
+                    error={adminLastNameErrors > 0}
+                    helperText={adminLastNameErrors.length > 0 ? (
+                      adminLastNameErrors[0]
+                    ) : (null)
+                    }
                   />
-                  {this.state.adminLastNameErrors.length > 0 ? (
-                    this.state.adminLastNameErrors.map((element, i) => <p style={pstyle} key={i}>{element}</p>)
-                  ) : console.log('it was false')
-                  }
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label">
-                    Employee Type
-                    {' '}
-
-                  </label>
-                </div>
-
-                <div className="col-6 col-mr-auto">
+                </Grid>
+                <Grid xs={12}>
                   <FormControl className="col-12">
                     <InputLabel htmlFor="label">Select</InputLabel>
                     <Select
                       name="employeeType"
-                      value={this.state.employeeType}
+                      value={employeeType}
                       onChange={this.handleChange}
                       inputProps={{
                         id: 'label',
@@ -215,100 +187,65 @@ class AddUserModal extends React.Component {
                       className="col-12"
                       style={{ minWidth: '100%' }}
                     >
-
                       <MenuItem value="employee">employee</MenuItem>
-                      {/* <MenuItem value="manager">manager</MenuItem> */}
+                      <MenuItem value="manager">manager</MenuItem>
                       <MenuItem value="admin">admin</MenuItem>
                     </Select>
                   </FormControl>
-                  {this.state.employeeTypeErrors.length > 0 ? (
-                    this.state.employeeTypeErrors.map((element, i) => <p style={pstyle} key={i}>{element}</p>)
-                  ) : console.log('it was false')
+                  {employeeTypeErrors.length > 0 ? (
+                    employeeTypeErrors[0]
+                  ) : (null)
                   }
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label">
-                    Job Title
-                    {' '}
-
-                  </label>
-                </div>
-                {/* </div> */}
-              <div className="col-6 col-ml-auto">
-                <TextField
-                  className="form-input"
-                  type="text"
-                  id="position"
-                  name="position"
-                  placeholder="jobtitle"
-                  value={this.state.position}
-                  onChange={this.handleChange}
-                />
-              </div>
-              </div>
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label" htmlFor="password">Password </label>
-                </div>
-                <div className="col-6 col-mr-auto">
+                </Grid>
+                <Grid xs={12}>
                   <TextField
-                    className="form-input"
-                    placeholder="password"
+                    margin="normal"
+                    label="password"
                     type="password"
                     name="password"
-                    value={this.state.password}
+                    value={password}
                     onChange={this.handleChange}
+                    error={passwordErrors > 0}
+                    helperText={passwordErrors.length > 0 ? (
+                      passwordErrors[0]
+                    ) : (null)
+                    }
                   />
-                  {this.state.passwordErrors.length > 0 ? (
-                    this.state.passwordErrors.map((element, i) => <p style={pstyle} key={i}>{element}</p>)
-                  ) : console.log('it was false')
-                  }
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="col-6 col-ml-auto">
-                  <label className="form-label" htmlFor="passwordMatch">Re-enter password</label>
-                </div>
-                <div className="col-6 col-mr-auto">
+                </Grid>
+                <Grid xs={12}>
                   <TextField
-                    className="form-input"
+                    margin="normal"
                     type="password"
                     id="passwordMatch"
                     name="passwordMatch"
-                    placeholder="passwordMatch"
-                    value={this.state.passwordMatch}
+                    label="passwordMatch"
+                    value={passwordMatch}
                     onChange={this.handleChange}
+                    error={passwordMatchErrors > 0}
+                    helperText={passwordMatchErrors.length > 0 ? (
+                      passwordMatchErrors[0]
+                    ) : (null)
+                    }
                   />
-                  {this.state.passwordMatchErrors.length > 0 ? (
-                    this.state.passwordMatchErrors.map((element, i) => <p style={pstyle} key={i}>{element}</p>)
-                  ) : console.log('it was false')
-                  }
-                </div>
-              </div>
-              <br />
-              <div className="form-group ">
-                <div className="col-12" />
-                <Button
-                  className="btn btn-primary col-12 col-mr-auto"
-                  onClick={this.handleSubmit}
-                  type="submit"
-                >
+                </Grid>
+                <br />
+                <Grid xs={12}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={this.handleSubmit}
+                    type="submit"
+                  >
                   Add User
-
-                </Button>
-              </div>
-            </form>
-          </div>
-        </Modal>
-      </div>
-    );
-  }
+                  </Button>
+                </Grid>
+              </form>
+            </div>
+          </Modal>
+        </div>
+      );
+    }
 }
 
-AddUserModal.propTypes = {
-  adminUsername: PropTypes.string.isRequired,
-};
 
 export default AddUserModal;
